815ac787a2f241c33d3ffe24b4b9e7813af86ead
Kristiyan Tsaklev <cuki@cukii.tk>
2017-03-03 17:09:45 +0200

    Update color only color changes

--- diffpp.cpp
+++ diffpp.cpp
@@ -2,18 +2,21 @@
 #include<fstream>
 #include<vector>
 
-std::string read_file(const char* filename)
-{
+std::string read_file(const char* filename) {
 	std::ifstream file(filename);
 
 	return std::string(std::istreambuf_iterator<char>(file),
 			std::istreambuf_iterator<char>());
 }
 
-int main(int argc, char** argv)
-{
-	if(argc != 3)
-	{
+enum {
+	BOTH,
+	FIRST,
+	SECOND
+} current_color;
+
+int main(int argc, char** argv) {
+	if(argc != 3) {
 		std::cerr << "Give two files\n";
 		return 1;
 	}
@@ -26,48 +29,32 @@ int main(int argc, char** argv)
 		for(unsigned j = str2.size(); j > 0; --j)
 			dp[i - 1][j - 1] = str1[i - 1] == str2[j - 1] ? dp[i][j] + 1 : std::max(dp[i][j - 1], dp[i - 1][j]);
 
-	for(unsigned i = 0, j = 0; i < str1.size() && j < str2.size();)
-	{
-		if(i < str1.size() && j < str2.size() && str1[i] == str2[j])
-		{
+	for(unsigned i = 0, j = 0; i < str1.size() && j < str2.size();) {
+		if(i < str1.size() && j < str2.size() && str1[i] == str2[j]) {
 			// color normal
-			std::cout << "\033[0m";
+			if(current_color != BOTH) {
+				current_color = BOTH;
+				std::cout << "\033[0m";
+			}
 			std::cout << str1[i];
 			++i; ++j;
-			continue;
 		}
-
-		if(i == str1.size())
-		{
+		else if(i == str1.size() || dp[i][j] != dp[i + 1][j]) {
 			// color red
-			std::cout << "\033[0;31m";
+			if(current_color != SECOND) {
+				current_color = SECOND;
+				std::cout << "\033[0;31m";
+			}
 			std::cout << str2[j];
 			++j;
-			continue;
-		}
-
-		if(j == str2.size())
-		{
+		} else {
 			// color green
-			std::cout << "\033[0;32m";
+			if(current_color != FIRST) {
+				current_color = FIRST;
+				std::cout << "\033[0;32m";
+			}
 			std::cout << str1[i];
 			++i;
-			continue;
-		}
-
-		if(dp[i][j] == dp[i + 1][j])
-		{
-			// color green
-			std::cout << "\033[0;32m";
-			std::cout << str1[i];
-			++i;
-		}
-		else
-		{
-			// color red
-			std::cout << "\033[0;31m";
-			std::cout << str2[j];
-			++j;
 		}
 	}
 }
